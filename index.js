6d18f2a216e233091244d5e0e84c10d31d84d8bd
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-04-04 17:48:42 +0200

    Data Movies update

--- index.js
+++ index.js
@@ -1,19 +1,61 @@
-/* const express = require('express'); */
-/* const app = express();*/
-const http = require('http');
-  url = require('url');
-  
-http.createServer((request, response) => {
-  var requestURL = url.parse(request.url, true);
-  if ( requestURL.pathname == "/documentation.html") {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Documentation on the book club API.\n');
-}   else {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('welcome to my book club!\n');
+const express = require('express');
+const app = express();
 
+let topMovies = [ {
+  title : 'Harry Potter and the Sorcerer\'s Stone'
+  ,
+  director : 'Chris Columbus'
+},
+{
+  title: 'Back To The Future',
+  director: 'Robert Zemeckis'
+},
+{
+  title: 'Jurassic Park',
+  director: 'Steven Spielberg'
+},
+{
+  title: 'Romeo \+ Julia',
+  director: 'Baz Luhrmann'
+},
+{
+  title: 'Starwars, The Rice of Skywalker',
+  director: 'J.J. Abrams'
+},
+{
+  title: 'Jurassic Park',
+  director: 'Steven Spielberg ao'
+},
+{
+  title: 'Pulp Fiction',
+  director: 'Quentin Tarantino'
+},
+{
+  title: 'Jurassic Park',
+  director: 'Steven Spielberg ao'
+},
+{
+  title: 'The Lion King',
+  director: 'Jon Favreau'
+},
+{
+  title: 'Spiderman, Far From Home',
+  director:'Jon Watts'
 }
+]
 
-}).listen(8080);
+// GET requests
+app.get('/', function(req, res) {
+  res.send('Welcome to my film club!')
+});
+app.get('/documentation', function(req, res) {
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+app.get('/movies', function(req, res) {
+  res.json(topMovies)
+});
 
-console.log('My first Node test server is running on Port 8080.');
+// listen for requests
+app.listen(8080, () =>
+ console.log('My Movie app is listening on port 8080')
+);
