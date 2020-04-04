9685c8daa88f150b7f0509e55795e18ab0941811
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-04-04 13:44:05 +0200

    test request different URL's

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 /* const express = require('express'); */
 /* const app = express();*/
 const http = require('http');
+  url = require('url');
+  
 http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('welcome to my book club!\n');
+  var requestURL = url.parse(request.url, true);
+  if ( requestURL.pathname == "/documentation.html") {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Documentation on the book club API.\n');
+}   else {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('welcome to my book club!\n');
+
+}
+
 }).listen(8080);
 
 console.log('My first Node test server is running on Port 8080.');
