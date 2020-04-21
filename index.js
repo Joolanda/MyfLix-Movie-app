d904dac3f69b430885d7bb89d65cbe88bd508f3b
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-04-21 15:21:23 +0200

    Add user from db using mongoosee

--- index.js
+++ index.js
@@ -13,208 +13,6 @@ const app = express();
 mongoose.connect('mongodb://localhost:27017/myFlixDB', {
 useNewUrlParser: true, useUnifiedTopology: true});
 
-let movies = [ {
-  id: '1',
-  title : "Harry Potter and the Philosopher\'s Stone",
-  description : 'Harry Potter has lived under the stairs at his aunt and uncle\'s house his whole life. But on his 11th birthday, he learns he\'s a powerful wizard -- with a place waiting for him at the Hogwarts School of Witchcraft and Wizardry. As he learns to harness his newfound powers with the help of the school\'s kindly headmaster, Harry uncovers the truth about his parents\' deaths and about the villain who\'s to blame.',
-  genre : 'Fantasy',
-  director : 'Chris Columbus',
-  imagePath : '',
-  featured : 'true'
-},
-{
-  id : '2',
-  title : 'Back To The Future',
-  description : 'It stars Michael J. Fox as teenager Marty McFly, who accidentally travels back in time, where he meets his future parents and becomes his mother\'s romantic interest.',
-  genre : 'Science Fiction',
-  director : 'Robert Zemeckis',
-  imagePath : '',
-  featured : 'true'
-},
-{
-  id : '3',
-  title : 'Jurassic Park',
-  description : 'The film is set on the fictional island of Isla Nublar, located off Central America\'s Pacific Coast near Costa Rica. There,  wealthy businessman John Hammond and a team of genetic scientists have created a wildlife park of de-extinct dinosaurs.',
-  genre : 'Adventure',
-  director : 'Steven Spielberg',
-  imagePath : '',
-  featured : 'true'
-},
-{
-  id : '4',
-  title: 'Romeo + Juliet',
-  description: 'In director Baz Luhrmann\'s contemporary take on William Shakespeare\'s classic tragedy, the Montagues and Capulets have moved their ongoing feud to the sweltering suburb of Verona Beach, where Romeo and Juliet fall in love and secretly wed. Though the film is visually modern, the bard\'s dialogue remains.',
-  genre : 'Drama',
-  director: 'Baz Luhrmann',
-  imagePath : '',
-  featured : 'true'
-},
-{
-  id : '5',
-  title : 'Starwars, The Rice of Skywalker',
-  description : 'The surviving Resistance faces the First Order once again as the journey of Rey, Finn and Poe Dameron continues. With the power and knowledge of generations behind them, the final battle begins.',
-  genres : 'Science Fiction',
-  director : 'J.J. Abrams',
-  imagePath : '',
-  featured : 'true'
-},
-{
-  id : '6',
-  title : 'The Blues Brothers',
-  description : 'Jake Blues is just out of jail, and teams up with his brother, Elwood on a \'mission from God\' to raise funds for the orphanage in which they grew up. The only thing they can do is do what they do best: play music. So they get their old band together, and set out on their way—while getting in a bit of trouble here and there.',
-  genres : 'Comedy',
-  director : 'John Landis',
-  imagePath : '',
-  featured : 'true'
-},
-{
-  id : '7',
-  title : 'Pulp Fiction',
-  description : 'A burger-loving hit man, his philosophical partner, a drug-addled gangster\'s moll and a washed-up boxer converge in this sprawling, comedic crime caper. Their adventures unfurl in three stories that ingeniously trip back and forth in time.',
-  genres : 'Crime',
-  director : 'Quentin Tarantino',
-  imagePath : '',
-  featured : 'true'
-},
-{
-  id : '8',
-  title : 'Interstellar',
-  description : 'Interstellar chronicles the adventures of a group of explorers who make use of a newly discovered wormhole to surpass the limitations on human space travel and conquer the vast distances involved in an interstellar voyage.',
-  genre : 'Epic',
-  director : 'Christopher Nolan',
-  imagePath : '',
-  featured : 'true'
-},
-{
-  id: '9',
-  title: 'The Lion King',
-  description : 'Simba idolizes his father, King Mufasa, and takes to heart his own royal destiny. But not everyone in the kingdom celebrates the new cub\'s arrival. Scar, Mufasa\'s brother—and former heir to the throne—has plans of his own. The battle for Pride Rock is ravaged with betrayal, tragedy and drama, ultimately resulting in Simba\'s exile. With help from a curious pair of newfound friends, Simba will have to figure out how to grow up and take back what is rightfully his.',
-  genre : 'Computer animated',
-  director: 'Jon Favreau',
-  imagePath : '',
-  featured : 'true'
-},
-{
-  id: '10',
-  title: 'Spiderman, Far From Home',
-  description : 'Peter Parker and his friends go on a summer trip to Europe. However, they will hardly be able to rest - Peter will have to agree to help Nick Fury uncover the mystery of creatures that cause natural disasters and destruction throughout the continent.',
-  genre : 'Action',
-  director:'Jon Watts',
-  imagePath : '',
-  featured : 'true'
-}
-];
-
-let genres = [ {
-      name : 'Fantasy',
-      description : 'A fantasy story is about magic or supernatural forces,... '
-  },
-  {
-      name : 'Science Fiction',
-      description : 'Science fiction film (or sci-fi film) is a genre that ... '
-  },
-  {
-      name : 'Adventure',
-      description : 'Adventure films are a genre of film that typically use their action... '
-  },
-  {
-      name : 'Drama',
-      description : 'Dramas are serious, plot-driven presentations, portraying realistic characters,... '
-  },
-  {
-      name : 'Comedy',
-      description : 'Comedies are light-hearted plots consistently and deliberately designed to amuse... '
-  },
-  {
-      name : 'Crime',
-      description : 'Crime films, in the broadest sense, are a film genre inspired by and ... '
-  },
-  {
-      name : 'Epic',
-      description : 'Epic films are a style of filmmaking with large scale, sweeping scope, and spectacle... '
-  },
-  {
-      name : 'Computer animated',
-      description : 'Computer animation is the process used for digitally generating animated images... '
-  },
-  {
-      name : 'Action',
-      description : 'Action films usually include high energy, big-budget physical stunts and chases, ... '
-  }
-];
-
-let directors = [ {
-    name : 'Chris Columbus',
-    bio : 'Christopher Joseph Columbus is an American film director, …, for Best Comedy Film. ',
-    birth: '1958',
-   death: ' '
-},
-{
-    name : 'Robert Zemeckis',
-    bio :  ' Robert Lee Zemeckis is an American director, …, has used special effects to more dramatic and narrative purpose.',
-    birth: '1951',
-    death: ' '
-},
-{
-    name : 'Steven Spielberg',
-    bio :  'Steven Allan Spielberg is considered one of the… most popular directors and producers in film history.',
-    birth: '1946',
-    death: ' '
-},
-{
-    name : 'Baz Luhrmann',
-    bio :  'Mark Anthony "Baz" Luhrmann is an Australian film director, …and Nicole Kidman. ',
-    Birth: '1962',
-    death: ' '
-},
-{
-    name : 'J.J. Abrams',
-    bio :  'Abrams has created numerous television series, including Felicity, …the saga, Star Wars: The Rise of Skywalker (2019).',
-    Birth: '1966',
-    death: ' '
-
-},
-{
-    name : 'John Landis',
-    bio :  'John Landis is an American film director, screenwriter, actor, …, music videos with singer Michael Jackson.',
-    Birth: '1950',
-    death: ' '
-},
-{
-    name : 'Quentin Tarantino',
-    bio :  'Quentin Jerome Tarantino is an American film director, screenwriter, …, born in Queens, New York.',
-    Birth: '1963',
-    death: ' '
-},
-{
-    name : 'Christopher Nolan',
-    bio :  'Christopher Edward Nolan, is a British-American film director, …, and his brother, screenwriter Jonathan Nolan.',
-    Birth: '1970',
-    death: ' '
-},
-{
-    name : 'Jon Favreau',
-    bio :  'Jonathan Favreau is an American actor, director, …., as co-producers in most of Favreau\'s directorial ventures.',
-    Birth: '1966',
-    death: ' '
-},
-{
-    name : 'Jon Watts',
-    bio :  'Jon Watts is an American film director, producer and… ,… on his chest to make himself "stand out in the field.',
-    Birth: '1981',
-    death: ' '
-}
-];
-let users = [ {
-  id : '0',
-  username : 'Ruby Took',
-  password : 'peiQu0hi4',
-  email : 'RubyTook@teleworm.us',
-  birthday : '1988-03-31',
-  favorites : ['8']
-  }
-];
-
 // use express.static
 //to serve “documentation.html” file from the public folder
 app.use(express.static('public'));
@@ -272,9 +70,40 @@ app.get('/users/:username', (req, res) => {
   res.json(users.find( (user) =>
     { return user.username ===req.params.username  }));
 });
-//Posts a new user to our list of users
+// Add a user from DB using mongoose models - (replace code: Posts a new user to our list of users)
+/* We'll expect JSON in this format
+{
+  ID: Integer,
+  Username: String,
+  Password: String,
+  Email: String,
+  Birthday: Date
+}*/
 app.post('/users', (req, res) => {
-  let newUser = req.body;
+  Users.findone({ Username: req.body.Username })
+    .then((user) => {
+      if(user) {
+        return res.status(400).send(req.body.Username +'already exits');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) =>{res.status(201).json(user) })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        })
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+  });
 
   if (!newUser.username) {
     const message = 'Missing name in request body';
