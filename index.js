95dd73b1f7bbdc090946ef48ae0b91a21ede603a
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-04-22 16:32:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -182,7 +182,7 @@ app.put('/users/:Username', (req, res) => {
   });
 
 // ADD a movie to a user's list of favorites
-app.post('/users/:Username/movies/:MovieID', (req, res) =>
+app.post('/users/:Username/Movies/:MovieID', (req, res) =>
   {
     Users.findOneAndUpdate({ Username: req.params.Username
   }, {
