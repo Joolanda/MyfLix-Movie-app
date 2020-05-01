7e24d6f11d2511e5f72906a547adc8a542f8e9eb
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-05-01 15:17:15 +0200

    connection MogooseDBAtlas

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 //  useUnifiedTopology: true
 //});
 
-mongoose.connect('mongodb+srv://myStorageDBadmin:12345@mystoragedb-1xpkf.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://myStorageDBadmin:12345@mystoragedb-1xpkf.mongodb.net/myFlixDB?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
