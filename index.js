0d8962afa1bed640aab4d93088596b1850f082bd
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-05-06 23:18:47 +0200

    editing

--- index.js
+++ index.js
@@ -42,13 +42,14 @@ app.use(cors({
       if(allowedOrigins.indexOf(origin) === -1) {
         // If a specific origin isn't found on the list of allowed origins:
         let message =
-          'The CORS policy for this application doesn´t allow access from origin ' +
+          'The CORS policy for this application doesnt allow access from origin ' +
           origin;
         return callback(new Error(message), false);
       }
       return callback(null, true);
-    }
-  }));
+    },
+  })
+);
 
 // get requests
 app.get('/', function (req, res) {
