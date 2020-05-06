081aac64969036cb494157a29a61d3f7163851fb
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-05-06 22:42:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const { check, validationResult } = require('express-validator');
 const passport = require('passport');
 require('./passport');
 
+//mongoose.connect('mongodb://127.0.0.1:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
