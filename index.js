ac769992e3e74d070f829114f63be8ea157372ed
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-05-06 22:55:11 +0200

    beautify code

--- index.js
+++ index.js
@@ -36,21 +36,19 @@ let allowedOrigins = [
   'https://myflix-movie-25.herokuapp.com'
 ];
 // CORS implementation
-app.use(
-  cors({
+app.use(cors({
     origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1) {
         // If a specific origin isn't found on the list of allowed origins:
         let message =
-          'The CORS policy for this application doesn\'t allow access from origin ' +
+          'The CORS policy for this application doesn´t allow access from origin ' +
           origin;
         return callback(new Error(message), false);
       }
       return callback(null, true);
-    },
-  })
-);
+    }
+  }));
 
 // get requests
 app.get('/', function (req, res) {
