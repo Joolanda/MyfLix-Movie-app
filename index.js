5c88b48243bcf912e921660fe91933cc4e5bda7f
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-05-09 20:48:19 +0200

    check packagea

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ const Users = Models.User;
 const cors = require('cors');
 
 //Mongoose db connections
-//require('dotenv').config();
+
 //mongoose.connect('mongodb://127.0.0.1:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connect( process.env.CONNECTION_URI, {useNewUrlParser: true,useUnifiedTopology: true});
+mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser: true,useUnifiedTopology: true});
 
 // CORS origin sites to be given access:
 let allowedOrigins = [
@@ -55,7 +55,7 @@ let auth = require('./auth')(app); // place auth.js file after bodyParser middle
 
 // GET requests
 app.get('/', function (req, res) {
-  var responseText = 'Welcome to my filmclubje!'
+  var responseText = 'Welcome to my film club!!'
   res.send(responseText);
 });
 
