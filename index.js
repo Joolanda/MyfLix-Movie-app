6e3611e2b354c977cb7ddf1d51c61c78e8b8806c
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-05-12 18:16:41 +0200

    connection env

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ const cors = require('cors');
 
 //Mongoose db connections
 
-mongoose.connect('mongodb://127.0.0.1:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true});
-//mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser: true,useUnifiedTopology: true});
+//mongoose.connect('mongodb://127.0.0.1:27017/myFlixDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.CONNECTION_URI, {useNewUrlParser: true,useUnifiedTopology: true});
 //mongoose.connect('mongodb+srv://myFlixDBadmin:xxxxx>@myflixdb-ojsjk.mongodb.net/myFlixDB?retryWrites=true&w=majority', {useNewUrlParser: true,useUnifiedTopology: true});
 
 // CORS origin sites to be given access:
