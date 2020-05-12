299cad735ed3961d04b08e03a8f9d785e4e94be4
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-05-12 14:16:47 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,34 +1,33 @@
-const jwtSecret = 'your_jwt_secret'; // same key as in JWTStrategy
-const jwt = require('jsonwebtoken');
+var jwtSecret = 'your_jwt_secret';
+var jwt = require ('jsonwebtoken');
 const passport = require('passport');
+require('./passport');
 
-require('./passport'); // local passportfile
-
-let generateJWTToken = (user) => {
-  return jwt.sign(user, jwtSecret, {
-    subject: user.Username, // this is the username generated in the JWT
-    expiresIn: '7d', // the token will expire in x days
-    algorithm: 'HS256' // to encode JWT's values
+function generateJWTToken(user){
+  return jwt.sign(user, jwtSecret,{
+    subject:user.Username,
+    expiresIn: '7d',
+    algorithm: 'HS256'
   });
 }
 
-/* POST login. */
+/ POST login /
 module.exports = (router) => {
-  router.post('/login', (req, res) => {
-    passport.authenticate('local', { session: false }, (error, user) => {
-      if (error || !user) {
-        return res.status(400).json({
-          message: 'Something is not right',
-          user: user
-        });
-      }
-      req.login(user, { session: false }, (error) => {
-        if (error) {
-          res.send(error);
-        }
-        let token = generateJWTToken(user.toJSON());
-        return res.json({user: user, token: token });
-      });
-    })(req, res);
-  });
-};
+ router.post('/login', (req, res) => {
+   passport.authenticate('local', { session : false}, (error, user, info) => {
+     if (error || !user) {
+       return res.status(400).json({
+         message: 'Something is not right',
+         user: user
+       });
+     }
+     req.login(user, { session: false }, (error) => {
+       if (error) {
+         res.send(error);
+       }
+       var token = generateJWTToken(user.toJSON());
+       return res.json({ user, token });
+     });
+   })(req, res);
+ });
+}
