ac4b2b70d4240eade403375a1ab51107fbafebd7
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-05-12 14:09:07 +0200

    Update models.js

--- models.js
+++ models.js
@@ -1,39 +1,39 @@
-const mongoose = require('mongoose'),
-bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-let movieSchema = mongoose.Schema({
-Title: {type: String, required: true},
-Description: {type: String, required: true},
-Genre: {
-  Name: String,
-  Description: String
+var movieSchema = mongoose.Schema({
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
+  Genre: {
+    Name: String,
+    Description: String
   },
-Director: {
-  Name: String,
-  Bio: String,
-  Birth: String
+  Director: {
+    Name: String,
+    Bio: String,
+    BirthYear: Number
   },
-//Actors: [String],
   ImagePath: String,
   Featured: Boolean
 });
 
-let userSchema = mongoose.Schema({
-  Username: {type: String, required: true},
-  Password: {type: String, required: true},
-  Email: {type: String, required: true},
+var userSchema = mongoose.Schema({
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
   Birthday: Date,
-  Favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+  Favourites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
-userSchema.statics.hashPassword = (password) => {
+
+userSchema.statics.hashPassword = function (password) {
   return bcrypt.hashSync(password, 10);
 };
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compareSync(password, this.Password);
+};
 
-userSchema.methods.validatePassword = (password) => {
-  return bcrypt.compareSync(password, this.Password)};
-
-let Movie = mongoose.model('Movie', movieSchema);
-let User = mongoose.model('User',userSchema);
+var Movie = mongoose.model('Movie', movieSchema);
+var User = mongoose.model('User', userSchema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;
