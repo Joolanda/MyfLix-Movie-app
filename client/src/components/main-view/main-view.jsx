5e9565e1906cadf3975f5b83703642ed992f56e8
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-05-18 22:44:07 +0200

    importing MovieCard

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -34,7 +34,7 @@ export class MainView extends React.Component {
     return (
      <div className="main-view">
      { movies.map(movie => (
-       <div className="movie-card" key={movie._id}>{movie.Title}</div>
+       <MovieCard key={movie._id}>{movie.Title}</div>
      ))}
      </div>
     );
