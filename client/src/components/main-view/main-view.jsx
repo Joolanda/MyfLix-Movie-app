c9e8ab54d1b6ab5e7483afe89c054dfe3a3c025b
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-06-22 10:30:28 +0200

    Update main-view.jsx

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -6,9 +6,10 @@ import { LoginView } from '../login-view/login-view';
 import { RegistrationView } from '../registration-view/registration-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+
+// bootstrap imports
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
 import { Button } from 'react-bootstrap';
 
 export class MainView extends React.Component {
