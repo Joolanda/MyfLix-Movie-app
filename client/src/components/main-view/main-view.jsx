76c15e886d572972f90d607cb6913a43a5472732
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-07-01 16:03:04 +0200

    Update main-view.jsx

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -9,8 +9,8 @@ import { LoginView } from '../login-view/login-view';
 import { RegistrationView } from '../registration-view/registration-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
-import { DirectorView } from '../director-view/director-view';
-import { GenreView } from '../genre-view/genre-view';
+//import { DirectorView } from '../director-view/director-view';
+//import { GenreView } from '../genre-view/genre-view';
 //import { ProfileView } from '../profile-view/profile-view';
 
 // bootstrap import
