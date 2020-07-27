2d52bed3ef07c6108fb2f068536ee6b55acd557d
Joolanda <50619917+Joolanda@users.noreply.github.com>
2020-07-27 10:04:34 +0200

    incl users interactions the app supports

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -1,12 +1,13 @@
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
+export const SET_USERS = ' SET_USERS';
 
 export function setMovies(value){
   return { type: SET_MOVIES, value };
 }
 
-export function setusers(value) {
-  return { type: set_users. value };
+export function setUsers(value) {
+  return { type: SET_USERS, value };
 }
 
 export function setFilter(value){
