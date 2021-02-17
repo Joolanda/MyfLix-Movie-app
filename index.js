af417cf3799d856f2b022a7941f9a551671bf7cf
Joolanda <50619917+Joolanda@users.noreply.github.com>
2021-02-17 20:43:39 +0100

    del cache files

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/client/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 // routes all requests for the client to 'dist' folder..next lines are for handlesubmit loginview task 3.5
-//  app.use('/client', express.static(path.join(__dirname, 'client', 'dist'))); 
+//  app.use('/client', express.static(path.join(__dirname, 'client', 'dist')));
 // all routes to the React client
 // app.get('/client/*', (req, res) => {
 // res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
@@ -47,7 +47,7 @@ app.use(
       if (!origin) return callback(null, true);
       if (allowedOrigins.indexOf(origin) === -1) {
         // If a specific origin isn't found on the list of allowed origins:
-        const message = 'The CORS policy for this application doesnt allow access from origin' + origin;;
+        const message = 'The CORS policy for this application doesnt allow access from origin' + origin;
         return callback(new Error(message), false);
       }
       return callback(null, true);
